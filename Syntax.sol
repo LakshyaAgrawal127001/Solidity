38e398e5afe5fdc05243db58ca4cdc6dfbffd716
Lakshya Agrawal <boypc3539@gmail.com>
2023-06-03 11:33:28 +0530

    Create Syntax.sol

---
+++ Syntax.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity  0.8.20;
+contract Hello{
+ 
+ uint public  num = 10; // state variable-> permant store on blockchain
+
+
+// 0xd92298c269E286c5bB65ADfFf0819421659C82f8
+}
+
+
+/*[  ABI code
+	{
+		"inputs": [],
+		"name": "num",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]*/
